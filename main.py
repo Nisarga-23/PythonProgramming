693f41bb45c499be42c2c4a189400494ac032161
Nisarga-23 <117572409+Nisarga-23@users.noreply.github.com>
2022-11-19 14:42:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# startpoint = int(input("Enter the start point: "))
+# endpoint = int(input("Enter the end point: "))
+# sumofnumbers = 0
+# for i in range(startpoint, endpoint):
+#     sumofnumbers = sumofnumbers + i
+# print("The sum of numbers between {} and {} is {}".format(startpoint, endpoint,sumofnumbers))
+#
+# startpoint = int(input("Enter the start point: "))
+# endpoint = int(input("Enter the end point: "))
+# sumofnumbers = 0
+# v=1
+# for i in range(startpoint, endpoint +1,v ):
+#     print("The sum of the number between {} and {} is {}" .format(startpoint, endpoint, v, sumofnumbers))
+# startpoint = int(input("Enter the start point: "))
+# endpoint = int(input("Enter the end point: "))
+# sumofnumbers = 0
+# stepnumber= 1
+# for i in range(startpoint, endpoint+1, stepnumber):
+#     # print("The sum of the number between {} and {} is {}" .format(startpoint, endpoint, stepnumber, sumofnumbers
+#    print(i)
+startpoint = -5
+endpoint = -1
+for number in range(startpoint, endpoint+1):
+    print(number)
+else:
+    print("The loop has ended")
